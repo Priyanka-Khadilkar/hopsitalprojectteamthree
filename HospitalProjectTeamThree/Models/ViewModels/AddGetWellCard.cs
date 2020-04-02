243e4a3cfbca6d44cac4be6e6edf93c0e46c24b8
vietphuongtran <55298689+vietphuongtran@users.noreply.github.com>
2020-04-02 11:49:22 -0400

    fixing List GetWell

---
+++ HospitalProjectTeamThree/Models/ViewModels/AddGetWellCard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HospitalProjectTeamThree.Data;
+
+namespace HospitalProjectTeamThree.Models.ViewModels
+{
+    public class AddGetWellCard
+    {
+        //We need a list of card design to put in radio box
+        public virtual List<CardDesign> CardDesigns { get; set; }
+
+    }
+}
