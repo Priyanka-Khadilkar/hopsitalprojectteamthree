21d4554b314a11f317d42ae35418f536a019c567
Priyanka Khadilkar <priya@LAPTOP-ETV65Q7J>
2020-04-01 21:59:30 -0400

    Events Added

---
+++ HospitalProjectTeamThree/Migrations/202004020144071_events_users_many_to_many.cs
@@ -0,0 +1,16 @@
+﻿namespace HospitalProjectTeamThree.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class events_users_many_to_many : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ HospitalProjectTeamThree/Models/Event.cs
@@ -0,0 +1,36 @@
+﻿using HospitalProjectTeamThree.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace HospitalProjectTeamThree.Models
+{
+    public class Event
+    {
+        [Key]
+        public int EventId { get; set; }
+        public int EventTitle { get; set; }
+        public DateTime EventStartDate { get; set; }
+        public DateTime EventEndDate { get; set; }
+        public string EventTime { get; set; }
+        public string EventLocation { get; set; }
+        public string EventTargetAudience { get; set; }
+        public string EventHostedBy { get; set; }
+        public string EventImagePath { get; set; }
+        public string EventDetails { get; set; }
+        public string EventCreatedBy { get; set; }
+        [ForeignKey("EventCreatedBy")]
+        public virtual ApplicationUser EventCreater { get; set; }
+        public string EventUpdatedBy { get; set; }
+        [ForeignKey("EventUpdatedBy")]
+        public virtual ApplicationUser EventUpdater { get; set; }
+        public DateTime EventCreatedOn { get; set; }
+        public DateTime EventUpdatedOn { get; set; }
+
+        public virtual ICollection<ApplicationUser> Users { get; set; }
+
+    }
+}
