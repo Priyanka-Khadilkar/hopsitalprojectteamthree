57cc808dae34d0626d53e41c873b60edb0b7b34c
vietphuongtran <55298689+vietphuongtran@users.noreply.github.com>
2020-04-04 12:18:15 -0400

    changing get well card class

---
+++ HospitalProjectTeamThree/Migrations/202004041614068_remove redundant data get well soon card.cs
@@ -0,0 +1,20 @@
+﻿namespace HospitalProjectTeamThree.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class removeredundantdatagetwellsooncard : DbMigration
+    {
+        public override void Up()
+        {
+            RenameColumn(table: "dbo.GetWellSoonCards", name: "CardUserId", newName: "CardUsers_Id");
+            RenameIndex(table: "dbo.GetWellSoonCards", name: "IX_CardUserId", newName: "IX_CardUsers_Id");
+        }
+        
+        public override void Down()
+        {
+            RenameIndex(table: "dbo.GetWellSoonCards", name: "IX_CardUsers_Id", newName: "IX_CardUserId");
+            RenameColumn(table: "dbo.GetWellSoonCards", name: "CardUsers_Id", newName: "CardUserId");
+        }
+    }
+}
