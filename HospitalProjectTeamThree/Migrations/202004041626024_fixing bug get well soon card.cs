a22aba8d809d371e993736f70ff2054cf320ce0d
vietphuongtran <55298689+vietphuongtran@users.noreply.github.com>
2020-04-04 12:28:21 -0400

    fixing bug in get well soon card class

---
+++ HospitalProjectTeamThree/Migrations/202004041626024_fixing bug get well soon card.cs
@@ -0,0 +1,16 @@
+﻿namespace HospitalProjectTeamThree.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class fixingbuggetwellsooncard : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
