dc1eb6cf0d8c129b515b05a9eb3ff5991dac76f7
VitaliyBulyma <55520185+VitaliyBulyma@users.noreply.github.com>
2020-04-03 23:20:28 -0400

    crisisCrud

--- HospitalProjectTeamThree/Controllers/ArticleController.cs
+++ HospitalProjectTeamThree/Controllers/ArticleController.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
-using System.Diagnostics;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using HospitalProjectTeamThree.Data;
 using HospitalProjectTeamThree.Models;
+using HospitalProjectTeamThree.Models.ViewModels;
+using System.Diagnostics;
+using System.IO;
 
 namespace HospitalProjectTeamThree.Controllers
 {
@@ -34,8 +39,28 @@ namespace HospitalProjectTeamThree.Controllers
         }
         public ActionResult CreateArticle()
         {
-            return View();
+            List<Crisis> crises = db.Crises.SqlQuery("select * from Crises").ToList();
+            return View(crises);
         }
+        [HttpPost]
+
+        public ActionResult CreateArticle(string ArticleAuthor, string ArticleTitle, string ArticleContent, DateTime DatePosted, int CrisisId)
+        {
+
+            Debug.WriteLine("Value of variables are " + ArticleAuthor + ArticleTitle + ArticleContent  + DatePosted);
+
+            string query = "insert into Articles (ArticleTitle, ArticleAuthor, ArticleContent, DatePosted, Crisis_CrisisId) values (@ArticleTitle, @ArticleAuthor, @ArticleContent, @DatePosted, @Crisis_CrisisId)";
+            SqlParameter[] sqlparams = new SqlParameter[5];
+            sqlparams[0] = new SqlParameter("@ArticleTitle", ArticleTitle);
+            sqlparams[1] = new SqlParameter("@ArticleAuthor", ArticleAuthor);
+            sqlparams[2] = new SqlParameter("@ArticleContent", ArticleContent);
+            sqlparams[3] = new SqlParameter("@Crisis_CrisisId", CrisisId);
+            sqlparams[4] = new SqlParameter("@DatePosted", DatePosted);
+            db.Database.ExecuteSqlCommand(query, sqlparams);
+
+            return RedirectToAction("List");
+        }
+
         public ActionResult Admin()
         {
             return View();
---
+++ HospitalProjectTeamThree/Controllers/CrisisController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using HospitalProjectTeamThree.Models;
+using System.Collections.Generic;
+using HospitalProjectTeamThree.Data;
+using System.Diagnostics;
+using System.Data.SqlClient;
+
+namespace HospitalProjectTeamThree.Controllers
+{
+    
+    public class CrisisController : Controller
+    {
+        private HospitalProjectTeamThreeContext db = new HospitalProjectTeamThreeContext();
+        // GET: Crisis
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CrisisList()
+        {
+            string query = "Select * from Crises ";
+            List<Crisis> crises = db.Crises.SqlQuery(query).ToList();
+            //Debug.WriteLine("Checking connection to database");
+            return View(crises);
+            
+        }
+        public ActionResult CreateCrisis()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult CreateCrisis(string CrisisName, DateTime CrisisStarted, DateTime CrisisFinished, string CrisisDesc)
+        {
+
+            Debug.WriteLine("Value of variables are " + CrisisName + CrisisStarted + CrisisFinished + CrisisDesc);
+           
+            string query = "insert into Crises (CrisisName, CrisisStrated, CrisisFinished, CrisisDesc) values (@CrisisName, @CrisisStrated, @CrisisFinished,@CrisisDesc )";
+            SqlParameter[] sqlparams = new SqlParameter[4];
+            sqlparams[0] = new SqlParameter("@CrisisName", CrisisName);
+            sqlparams[1] = new SqlParameter("@CrisisStrated", CrisisStarted);
+            sqlparams[2] = new SqlParameter("@CrisisFinished", CrisisFinished);
+            sqlparams[3] = new SqlParameter("@CrisisDesc", CrisisDesc);
+            db.Database.ExecuteSqlCommand(query, sqlparams);
+
+            return RedirectToAction("CrisisList");
+        }
+
+        public ActionResult UpdateCrisis(int id)
+        {
+            //retrieves info for a specific vehicle type
+            Crisis selectedcrisis = db.Crises.SqlQuery("select * from Crises where CrisisId = @id", new SqlParameter("@id", id)).FirstOrDefault();
+
+            return View(selectedcrisis);
+        }
+        [HttpPost]
+        public ActionResult UpdateCrisis(int id, string CrisisName, DateTime CrisisStarted, DateTime CrisisFinished, string CrisisDesc)
+        {
+
+            Debug.WriteLine("I am trying to display variables" + id + CrisisName + CrisisStarted + CrisisFinished + CrisisDesc);
+
+            // updates the record on the submission
+            string query = "update Crises SET  CrisisName=@CrisisName, CrisisStrated=@CrisisStrated, CrisisFinished=@CrisisFinished, CrisisDesc=@CrisisDesc where CrisisId=@id";
+
+
+            SqlParameter[] sqlparams = new SqlParameter[5];
+
+            sqlparams[0] = new SqlParameter("@CrisisName", CrisisName);
+            sqlparams[1] = new SqlParameter("@id", id);
+            sqlparams[2] = new SqlParameter("@CrisisStrated", CrisisStarted);
+            sqlparams[3] = new SqlParameter("@CrisisFinished", CrisisFinished);
+            sqlparams[4] = new SqlParameter("@CrisisDesc", CrisisDesc);
+
+
+            db.Database.ExecuteSqlCommand(query, sqlparams);
+            return RedirectToAction("CrisisList");
+        }
+        public ActionResult DeleteCrisis(int id)
+        {
+            //Deletes the record from the database
+            string query = "delete from Crises where CrisisId=@id";
+            SqlParameter sqlparam = new SqlParameter("@id", id);
+
+            db.Database.ExecuteSqlCommand(query, sqlparam);
+            return RedirectToAction("CrisisList");
+        }
+    }
+}
