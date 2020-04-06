788459607fce9ad4cfeb644c591473f0fda04b14
VitaliyBulyma <55520185+VitaliyBulyma@users.noreply.github.com>
2020-04-06 12:33:42 -0400

    Room views added

--- HospitalProjectTeamThree/Controllers/RoomController.cs
+++ HospitalProjectTeamThree/Controllers/RoomController.cs
@@ -12,6 +12,8 @@ using HospitalProjectTeamThree.Models;
 using System.Collections.Generic;
 using HospitalProjectTeamThree.Data;
 using System.Diagnostics;
+using System.Data.SqlClient;
+using HospitalProjectTeamThree.Models.ViewModels;
 
 namespace HospitalProjectTeamThree.Controllers
 {
@@ -31,5 +33,40 @@ namespace HospitalProjectTeamThree.Controllers
             //Debug.WriteLine("Checking connection to database");
             return View(rooms);
         }
+        public ActionResult AdminView()
+        {
+            return View();
+        }
+/*        public ActionResult ShowOne()
+        {
+
+            string query = "Select * from Rooms ";
+            List<Room> rooms = db.Rooms.SqlQuery(query).ToList();
+            //Debug.WriteLine("Checking connection to database");
+            return View(rooms);
+        }*/
+      
+        public ActionResult ShowOne(int? id)
+        {
+
+
+            //get all info about one room given the id
+            Room Room = db.Rooms.SqlQuery("select * from Rooms where RoomID=@RoomID", new SqlParameter("@RoomID", id)).FirstOrDefault();
+
+
+            //list all Rooms 
+            string query = "Select * from Rooms ";
+            List<Room> rooms = db.Rooms.SqlQuery(query).ToList();
+
+
+            FeaturedRoom viewmodel = new FeaturedRoom();
+            viewmodel.rooms = rooms;
+            viewmodel.room = Room;
+
+
+
+            return View(viewmodel);
+        }
+       
     }
 }
---
+++ HospitalProjectTeamThree/Models/ViewModels/FeaturedRoom.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalProjectTeamThree.Models.ViewModels
+{
+    public class FeaturedRoom
+    {
+        public virtual Room room { get; set; }
+        public List<Room> rooms { get; set; }
+
+
+    }
+}
