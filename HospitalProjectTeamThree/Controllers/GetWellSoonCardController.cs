3ae1d481e844f4d73c36352a6e84d052152f5909
vietphuongtran <55298689+vietphuongtran@users.noreply.github.com>
2020-04-07 21:58:36 -0400

    adding personal list of card

--- HospitalProjectTeamThree/Controllers/GetWellSoonCardController.cs
+++ HospitalProjectTeamThree/Controllers/GetWellSoonCardController.cs
@@ -17,6 +17,7 @@ using Microsoft.AspNet.Identity.Owin;
 using System.Web.Security;
 //need this for pagination
 using PagedList;
+using Microsoft.AspNet.Identity;
 
 namespace HospitalProjectTeamThree.Controllers
 {
@@ -82,6 +83,17 @@ namespace HospitalProjectTeamThree.Controllers
             int pageNumber = (page ?? 1);
             //passing the pagelist to the view
             return View(GetWellSoonCards.ToPagedList(pageNumber, pageSize));
+        }
+        public ActionResult PersonalList()
+        {
+            string userId = User.Identity.GetUserId();
+            ApplicationUser currentUser = db.Users.FirstOrDefault(x => x.Id == userId); 
+            string query = "Select * from GetWellSoonCards where UserId=@userId";
+            
+            SqlParameter[] sqlparams = new SqlParameter[1];
+            sqlparams[0] = new SqlParameter("@userId", userId);
+            db.Database.ExecuteSqlCommand(query);
+            return View();
         }
         public ActionResult Add()
         {
