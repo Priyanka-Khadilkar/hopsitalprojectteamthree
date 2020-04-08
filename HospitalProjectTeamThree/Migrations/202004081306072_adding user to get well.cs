b40f657d0d5f30cc6d780e4310165a9271f4beae
vietphuongtran <55298689+vietphuongtran@users.noreply.github.com>
2020-04-08 09:08:13 -0400

    adding user in get well soon card table

---
+++ HospitalProjectTeamThree/Migrations/202004081306072_adding user to get well.cs
@@ -0,0 +1,22 @@
+﻿namespace HospitalProjectTeamThree.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addingusertogetwell : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.GetWellSoonCards", "User_Id", c => c.String(maxLength: 128));
+            CreateIndex("dbo.GetWellSoonCards", "User_Id");
+            AddForeignKey("dbo.GetWellSoonCards", "User_Id", "dbo.AspNetUsers", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.GetWellSoonCards", "User_Id", "dbo.AspNetUsers");
+            DropIndex("dbo.GetWellSoonCards", new[] { "User_Id" });
+            DropColumn("dbo.GetWellSoonCards", "User_Id");
+        }
+    }
+}
