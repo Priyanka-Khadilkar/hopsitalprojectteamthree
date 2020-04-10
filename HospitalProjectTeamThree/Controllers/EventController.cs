91a378709b8d37599a3b44f8514c45f9a37c0fbe
Priyanka Khadilkar <priya@LAPTOP-ETV65Q7J>
2020-04-10 17:26:38 -0400

    Event - Listing add

--- HospitalProjectTeamThree/Controllers/EventController.cs
+++ HospitalProjectTeamThree/Controllers/EventController.cs
@@ -5,11 +5,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
+using System.IO;
+using HospitalProjectTeamThree.Data;
 
 namespace HospitalProjectTeamThree.Controllers
 {
     public class EventController : Controller
     {
+        private HospitalProjectTeamThreeContext db = new HospitalProjectTeamThreeContext();
         // GET: Event
         [Authorize(Roles = "Admin")]
         public ActionResult Add()
@@ -21,7 +24,7 @@ namespace HospitalProjectTeamThree.Controllers
         [ValidateInput(false)]
         [Authorize(Roles = "Admin")]
         public ActionResult Add(string EventTitle, string EventStartDate, string EventEndDate, string EventFromTime, string EventToTime, HttpPostedFileBase EventImage, string EventTargetAudience,
-            string EventHostedBy, string EventDetail)
+            string EventHostedBy, string EventDetail, string EventLocation)
         {
             Event eventModel = new Event();
             eventModel.EventTitle = EventTitle;
@@ -31,10 +34,48 @@ namespace HospitalProjectTeamThree.Controllers
             eventModel.EventTargetAudience = EventTargetAudience;
 
             string currentUserId = User.Identity.GetUserId();
-            eventModel.EventHostedBy = currentUserId;
+            eventModel.EventHostedBy = EventHostedBy;
             eventModel.EventDetails = EventDetail;
+            eventModel.EventCreatedOn = DateTime.Now;
+            eventModel.EventLocation = EventLocation;
 
-            return View();
+            //Save Images
+            string path = Path.Combine(Server.MapPath("~/Images/Event"),
+                                       Path.GetFileName(EventImage.FileName));
+            EventImage.SaveAs(path);
+            eventModel.EventImagePath = Path.GetFileName(EventImage.FileName);
+
+            //Get the current logged in user
+            eventModel.EventCreater = db.Users.FirstOrDefault(x => x.Id == currentUserId);
+            db.Events.Add(eventModel);
+
+            //Save data into database
+            db.SaveChanges();
+            return RedirectToAction("List");
         }
+
+        /// <summary>
+        /// List all Events
+        /// </summary>
+        /// <returns>returns list of Events </returns>
+        public ActionResult List()
+        {
+            List<Event> events = new List<Event>();
+            events = db.Events.Include("EventCreater").ToList();
+            //Return all bookings to the listing page
+            return View(events);
+        }
+
+        /// <summary>
+        /// List all Events
+        /// </summary>
+        /// <returns>returns list of Events </returns>
+        public ActionResult Show(int id)
+        {
+            Event eventdata = db.Events.Include("EventCreater").Where(x => x.EventId == id).FirstOrDefault();
+            //Return event detail
+            return View(eventdata);
+        }
+
     }
 }
