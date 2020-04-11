e08b4055f5612d58da419169445dbeece708a567
Alexa Perez <alexapg8@gmail.com>
2020-04-10 21:23:16 -0400

    Live Wait Times Feature updates

---
+++ HospitalProjectTeamThree/Models/ViewModels/ShowLiveWaitTimes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HospitalProjectTeamThree.Data;
+
+namespace HospitalProjectTeamThree.Models.ViewModels
+{
+    public class ShowLiveWaitTimes
+    {
+        public Department Departments { get; set; }
+        public List<LiveWaitTime> WaitTimes { get; set; }
+    }
+}
